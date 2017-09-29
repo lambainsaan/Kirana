d52d0abdae7fd5beccd6fe3e96d2623b238c5088
Rhitik Bhatt <bhattrhitik95@gmail.com>
2017-09-29 17:58:41 +0530

    Data Binding

--- src/app/shared/items.ts
+++ src/app/shared/items.ts
@@ -1,8 +1,70 @@
-export class Item {
-    name: string;
-    image: string;
-    category: string;
-    label: string;
-    price: string;
-    description: string;
-}
+import { Item } from '../shared/item';
+
+
+export const ITEMS: Item[] = [{
+    name: 'Colgate',
+    image: '/assets/images/toothpaste.jpg',
+    category: 'daily',
+    label: 'Discount',
+    price: '60',
+    description: 'A great tootpaste made by the company trusted for over years.'
+},
+{
+    name: 'Amul Ghee',
+    image: '/assets/images/amul-ghee.jpg',
+    category: 'grocery',
+    label: 'Featured',
+    price: '360',
+    description: 'Amul Ghee is clarified butter that is conventionally prepared from cow\'s\
+         milk.It is good source of power and provides vivacity to human body.'
+},
+{
+    name: 'Biseleri',
+    image: '/assets/images/bisleri.jpg',
+    category: 'beverage',
+    label: '',
+    price: '20',
+    description: 'A quintessential water bottle. Very yumm!?'
+},
+{
+    name: 'Amul Masti Dahi',
+    image: '/assets/images/dahi.jpg',
+    category: 'daily',
+    label: 'discount',
+    price: '20',
+    description: 'A delectable, semi-sweet New York Style\
+        Dahi fresh outta Gujrat!'
+},
+{
+    name: 'Maggi',
+    image: '/assets/images/maggi.jpg',
+    category: 'food',
+    label: 'offer',
+    price: '50',
+    description: 'The world famous noodles is here! Grab it before it runs out!'
+},
+{
+    name: 'Pears',
+    image: '/assets/images/pears.jpg',
+    category: 'soap|bath',
+    label: '',
+    price: '79',
+    description: 'The best soap with the essence of cardimon and water.'
+},
+{
+    name: 'Pepsi',
+    image: '/assets/images/pepsi.png',
+    category: 'beverage',
+    label: 'offer',
+    price: '25',
+    description: 'The beverage that everyone have lust for, its yumm and very disirable.'
+},
+{
+    name: 'Tomato',
+    image: '/assets/images/tomato.jpg',
+    category: 'grocery',
+    label: 'discount',
+    price: '25',
+    description: 'Specially chosen tomatos right from farms.'
+},
+];
