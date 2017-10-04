ecba9b5acc812d3fb4212eac6a3487748e466c28
Rhitik Bhatt <bhattrhitik95@gmail.com>
2017-10-04 14:41:17 +0530

    Moved the order form validation from template to component.

--- src/app/orderform/orderform.component.ts
+++ src/app/orderform/orderform.component.ts
@@ -13,6 +13,35 @@ export class OrderformComponent implements OnInit {
   orderForm: FormGroup;
   order: Order;
   slots = Slots;
+  formErrors = {
+    'name': '',
+    'mobileNum': '',
+    'address': '',
+    'landmark': '',
+    'slot': ''
+  };
+  validationMessages = {
+    'name': {
+      'required': 'Name is required.',
+      'minlength': 'Name must be at least 2 characters long.',
+      'maxlength': 'Name cannot be more than 50 characters long.'
+    },
+    'mobileNum': {
+      'required': 'Mobile number is required.',
+      'pattern': 'Mobile number must contain only numbers.'
+    },
+    'address': {
+      'required': 'Address is required.',
+      'minlength': 'Address must be at least 2 characters long.'
+    },
+    'landmark': {
+      'required': 'Landmark is required.',
+      'minlength': 'Landmark must be at least 2 characters long.'
+    },
+    'slot': {
+      'required': 'Slot is required.'
+    }
+  };
 
   constructor(private fb: FormBuilder) {
     this.createForm();
@@ -23,14 +52,38 @@ export class OrderformComponent implements OnInit {
 
   createForm() {
     this.orderForm = this.fb.group({
-      name: ['', Validators.required],
-      mobileNum: [0, Validators.required],
-      address: ['', Validators.required],
-      landmark: ['', Validators.required],
+      name: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(50)]],
+      mobileNum: [0, [Validators.required, Validators.pattern('[0-9]*')]],
+      address: ['', [Validators.required, Validators.minLength(5)]],
+      landmark: ['', Validators.required, Validators.minLength(3)],
       slot: ['', Validators.required],
       specialInstrcutions: '',
     });
+    this.orderForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); // (re)set validation messages now
+
   }
+
+  onValueChanged(data?: any) {
+    if (!this.orderForm) { return; }
+    const form = this.orderForm;
+    // tslint:disable-next-line:forin
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        // tslint:disable-next-line:forin
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
 
   onSubmit() {
     this.order = this.orderForm.value;
@@ -43,7 +96,7 @@ export class OrderformComponent implements OnInit {
       slot: '',
       specialInstrcutions: '',
     }
-);
+    );
   }
 
 }
